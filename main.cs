66f6abed06e0cd42c7fbf8773445608840b35398
Mrs. Boscombe <30632430+MrsBoscombe@users.noreply.github.com>
2020-10-11 16:19:31 -0400

    Create main.cs
    Initial commit

---
+++ main.cs
@@ -0,0 +1,15 @@
+/*  Filename main.cs
+    Author: Mrs. Boscombe
+    Lesson: Unit ## Lesson ##
+    Date: MM/DD/YYYY
+    Brief explanation of the purpose of the file
+*/
+
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    
+    }
+  }
+}
